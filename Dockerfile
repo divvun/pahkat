a485c1fa8db5a85035bd931fbe4b7c6286e670c3
Eike Siewertsen <eike@technocreatives.com>
2019-11-14 11:34:07 +0100

    simplify CLI, allow config from environment

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM rust:latest AS builder
+WORKDIR /build
+ADD . .
+RUN cargo build -p pahkat-server --release
+
+FROM debian:stretch-slim
+RUN apt-get update && apt-get install -y libsqlite3-dev
+WORKDIR /app/
+COPY --from=builder /build/target/release/pahkat-server .
+ENV ARTIFACTS_DIR artifacts
+ENV DATABASE_URL db/db.sqlite
+VOLUME artifacts db
+CMD ["./pahkat-server"]
--- pahkat-server/src/main.rs
+++ pahkat-server/src/main.rs
@@ -1,13 +1,17 @@
 extern crate diesel;
-#[macro_use] extern crate diesel_migrations;
+#[macro_use]
+extern crate diesel_migrations;
 
 use std::path::Path;
 use std::{env, fs};
 
-use clap::{crate_version, App as CliApp, AppSettings, Arg, ArgMatches, SubCommand};
+use clap::{
+    crate_authors, crate_description, crate_version, App as CliApp, AppSettings, Arg, ArgMatches,
+    SubCommand,
+};
 use log::{error, info, warn};
-
 use pahkat_common::ProgressOutput;
+use std::path::PathBuf;
 
 use config::TomlConfig;
 use server::run_server;
@@ -17,104 +21,104 @@ mod config;
 mod handlers;
 mod server;
 mod watcher;
+use failure::Error;
 
-fn main() {
+fn main() -> Result<(), Error> {
+    dotenv::dotenv().ok();
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
     let matches = CliApp::new("Páhkat server")
         .version(crate_version!())
-        .author("Rostislav Raykov <rostislav@technocreatives.com>")
-        .about("Páhkat server implementation")
+        .author(crate_authors!("\n"))
+        .about(crate_description!())
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(
-            SubCommand::with_name("run")
-                .about("Run the server")
-                .arg(
-                    Arg::with_name("path")
-                        .value_name("PATH")
-                        .help("The repository root directory (default: current working directory)")
-                        .short("p")
-                        .long("path")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("bind")
-                        .value_name("BIND")
-                        .help("The address which the server to listen to (default: 127.0.0.1)")
-                        .long("bind")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("port")
-                        .value_name("PORT")
-                        .help("The port which the server to listen to (default: 8000)")
-                        .long("port")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("config")
-                        .short("c")
-                        .long("config")
-                        .value_name("FILE")
-                        .help("Set a custom TOML config file")
-                        .required(true)
-                        .takes_value(true),
-                ),
+        .arg(
+            Arg::with_name("path")
+                .value_name("PATH")
+                .help("The repository root directory (default: current working directory)")
+                .short("p")
+                .long("path")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("bind")
+                .value_name("BIND")
+                .help("The address which the server to listen to (default: 127.0.0.1)")
+                .long("bind")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("port")
+                .value_name("PORT")
+                .help("The port which the server to listen to (default: 8000)")
+                .long("port")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Set a custom TOML config file")
+                .takes_value(true),
         )
         .get_matches();
 
-    match matches.subcommand() {
-        ("run", Some(matches)) => {
-            let current_dir = &env::current_dir().unwrap();
-            let path: &Path = matches
-                .value_of("path")
-                .map_or(&current_dir, |v| Path::new(v));
-            let bind: &str = matches.value_of("bind").map_or("127.0.0.1", |v| v);
-            let port: &str = matches.value_of("port").map_or("8000", |v| v);
+    let current_dir = &env::current_dir().unwrap();
+    let path: &Path = matches
+        .value_of("path")
+        .map_or(&current_dir, |v| Path::new(v));
+    let bind: &str = matches.value_of("bind").map_or("127.0.0.1", |v| v);
+    let port: &str = matches.value_of("port").map_or("8000", |v| v);
 
-            let mut watcher = Watcher::new(path).expect("Failed to start file watcher");
+    let mut watcher = Watcher::new(path).expect("Failed to start file watcher");
 
-            let output = ConsoleOutput;
+    let output = ConsoleOutput;
 
-            std::thread::spawn(move || {
-                let watcher_interval = std::time::Duration::from_millis(2000);
-                loop {
-                    match watcher.update() {
-                        Err(error) => error!("Failed to update watcher: {:?}", error),
-                        Ok(ref events) if !events.is_empty() => {
-                            info!(
-                                "Watcher reports {} event(s) since last update",
-                                events.len()
-                            );
-                            pahkat_common::repo_index(Path::new(watcher.path()), &output);
-                            // todo: repo_ops calls need improved error handling to support:
-                            // match repo_ops::repo_index(&path, &output) {
-                            //     Err(error) => eprintln!("Failed to re-index pahkat repo at {}: {:?}", watcher.path(), error),
-                            //     Ok(_) => println!("Successfully re-indexed pahkat repo at {}", watcher.path()),
-                            // }
-                        }
-                        _ => {}
-                    }
-                    std::thread::sleep(watcher_interval);
+    std::thread::spawn(move || {
+        let watcher_interval = std::time::Duration::from_millis(2000);
+        loop {
+            match watcher.update() {
+                Err(error) => error!("Failed to update watcher: {:?}", error),
+                Ok(ref events) if !events.is_empty() => {
+                    info!(
+                        "Watcher reports {} event(s) since last update",
+                        events.len()
+                    );
+                    pahkat_common::repo_index(Path::new(watcher.path()), &output);
+                    // todo: repo_ops calls need improved error handling to support:
+                    // match repo_ops::repo_index(&path, &output) {
+                    //     Err(error) => eprintln!("Failed to re-index pahkat repo at {}: {:?}", watcher.path(), error),
+                    //     Ok(_) => println!("Successfully re-indexed pahkat repo at {}", watcher.path()),
+                    // }
                 }
-            });
-
-            run_server(get_config(matches), path, bind, port);
+                _ => {}
+            }
+            std::thread::sleep(watcher_interval);
         }
-        _ => {}
-    }
+    });
+
+    run_server(get_config(&matches)?, path, bind, port);
+
+    Ok(())
 }
 
-fn get_config(matches: &ArgMatches<'_>) -> TomlConfig {
-    let config_file = matches.value_of("config").unwrap();
+fn get_config(matches: &ArgMatches<'_>) -> Result<TomlConfig, Error> {
+    if let Some(config_file) = matches.value_of("config") {
+        let config =
+            fs::read_to_string(&config_file).expect(&format!("Failed to open {}", config_file));
+        let config: TomlConfig =
+            toml::from_str(&config).expect(&format!("Failed to convert {} to TOML", config_file));
 
-    let config =
-        fs::read_to_string(&config_file).expect(&format!("Failed to open {}", config_file));
-    let config: TomlConfig =
-        toml::from_str(&config).expect(&format!("Failed to convert {} to TOML", config_file));
-
-    config
+        Ok(config)
+    } else {
+        Ok(TomlConfig {
+            artifacts_dir: PathBuf::from(env::var("ARTIFACTS_DIR")?),
+            url_prefix: env::var("URL_PREFIX")?,
+            db_path: env::var("DATABASE_URL").ok(),
+        })
+    }
 }
 
 struct ConsoleOutput;
