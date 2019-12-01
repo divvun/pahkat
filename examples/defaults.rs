4bba74a5af1e282a3be49d57ab7dc3284bbdb013
Brendan Molloy <brendan@technocreatives.com>
2019-12-01 20:30:10 +0100

    Add defaults printer example

---
+++ examples/defaults.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Config path: {:?}", pahkat_client::defaults::config_path());
+    println!("Tmp path: {:?}", pahkat_client::defaults::tmp_path());
+    println!("Cache path: {:?}", pahkat_client::defaults::cache_path());
+}
