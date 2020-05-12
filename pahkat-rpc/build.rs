8bc3eb1f6dff0d45420caddcda34b67b6bd52bbb
Pascal Hertleif <killercup@gmail.com>
2020-05-12 21:01:20 +0200

    Tell prost to add serde stuff
    Used this in another project and looks way less hacky.
    Still kept the serde rename.

--- pahkat-rpc/build.rs
+++ pahkat-rpc/build.rs
@@ -1,10 +1,19 @@
 fn main() {
-    tonic_build::compile_protos("proto/pahkat.proto").unwrap();
+    let proto_file = std::path::Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("proto/pahkat.proto");
+    let out_dir = std::env::var("OUT_DIR").unwrap();
 
-    let gen_path = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("pahkat.rs");
-    let data = std::fs::read_to_string(&gen_path).unwrap();
-    let data = data.replace("::prost::Message)", "::prost::Message, ::serde::Serialize, ::serde::Deserialize)");
-    let data = data.replace("::prost::Oneof)", "::prost::Oneof, ::serde::Serialize, ::serde::Deserialize)");
-    let data = data.replace("pub enum Value {", "#[serde(tag = \"type\")] pub enum Value {");
+    tonic_build::configure()
+        .out_dir(&out_dir)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(&[proto_file], &[])
+        .expect("tonic should build protos, but");
+
+    let gen_path = std::path::Path::new(&out_dir).join("pahkat.rs");
+    let data = std::fs::read_to_string(&gen_path).expect("`pahkat.rs` should exist, but");
+    let data = data.replace(
+        "pub enum Value {",
+        "#[serde(tag = \"type\")] pub enum Value {",
+    );
     std::fs::write(gen_path, data).unwrap();
 }
