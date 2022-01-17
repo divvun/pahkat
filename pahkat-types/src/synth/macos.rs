9dc4a3dd155deda679d50a902ad7159d40f97a3a
Brendan Molloy <brendan@technocreatives.com>
2022-01-17 19:59:41 +0100

    Finish fixing types...

--- pahkat-types/src/synth/macos.rs
+++ pahkat-types/src/synth/macos.rs
@@ -3,6 +3,7 @@ use typed_builder::TypedBuilder;
 #[derive(
     Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, TypedBuilder,
 )]
+#[cfg_attr(feature = "poem-openapi", derive(poem_openapi::Object))]
 pub struct PackageRef {
     #[serde(rename = "type")]
     #[builder(default = "MacOSPackageRef".into())]
@@ -19,6 +20,7 @@ pub struct PackageRef {
 #[derive(
     Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, TypedBuilder,
 )]
+#[cfg_attr(feature = "poem-openapi", derive(poem_openapi::Object))]
 pub struct PathRef {
     #[serde(rename = "type")]
     #[builder(default = "MacOSPathRef".into())]
--- pahkat-types/src/synth/windows.rs
+++ pahkat-types/src/synth/windows.rs
@@ -4,6 +4,7 @@ use typed_builder::TypedBuilder;
 #[derive(
     Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, TypedBuilder,
 )]
+#[cfg_attr(feature = "poem-openapi", derive(poem_openapi::Object))]
 pub struct RegistryKey {
     #[serde(rename = "type")]
     #[builder(default = "WindowsRegistryKey".into())]
