5d3b4a7f5bc39641b6552f12a68522f80ced2004
Bastien Orivel <bastien@technocreatives.com>
2022-03-16 16:21:53 +0100

    FINE, I'll do it myself

--- pahkat-client-core/build.rs
+++ pahkat-client-core/build.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
+use std::process::Command;
 
 fn main() -> Result<()> {
-    println!("cargo:rustc-env=GIT_VERSION={}", git_version::git_version!(args = ["--always", "--tags", "--dirty"]));
+    let output = String::from_utf8(Command::new("git").args(&["describe", "--always", "--tags", "--dirty"]).output()?.stdout)?;
+    println!("cargo:rustc-env=GIT_VERSION={}", output.trim());
     println!("cargo:rustc-env=CARGO_TARGET_TRIPLE={}", std::env::var("TARGET").unwrap_or_else(|_| "unknown".to_string()));
     fbs_build::compile_fbs("../pahkat-types/src/index.fbs")
 }
